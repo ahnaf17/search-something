fb8b69b225015d817c076f0ce6cf18fd4fd514b3
Ahnaf habib <habibahnaf17@gmail.com>
2024-05-27 01:54:42 +1000

    updated query term logic

--- src/lib/rest/SearchProduct.ts
+++ src/lib/rest/SearchProduct.ts
@@ -7,7 +7,7 @@ const parseResponse = (response: any): Product[] => (response.products.length ? 
 ) : []);
 
 const searchProduct = async (query: string): Promise<Product[]> => {
-  const term = query.split(' ').join('+');
+  const term = query.split(' ').filter(word => word !== '').join('+');
   const url = `https://dummyjson.com/products/search?q=${term}`;
   const response = await call(Method.GET, url, null);
   return parseResponse(response);
